f8505c7e2cce3363c5c26acf87d5a1da6d7682b9
Asus Tuf A15 <Asus Tuf A15@DESKTOP-R5OPB6J>
2023-08-13 01:37:55 +0300

    Добавьте файлы проекта.

---
+++ HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+class Program
+{
+    /// <summary>
+    /// Главный запускающий метод
+    /// </summary>
+    static void Main()
+    {
+
+        WriteLine("Hello World!!!"); //Вывод текста на экран
+        ReadKey(); //Ждем нажатия любой клавиши
+    }
+}
+
