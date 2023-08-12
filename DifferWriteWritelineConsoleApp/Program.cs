bc0c2c1cfbcf5bfd4df3aef91a341e7a3e5c49fa
Bikbaika <Kaleria4444@gmail.com>
2023-08-13 02:23:44 +0300

    Выполнение второго задания

---
+++ DifferWriteWritelineConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+class Program
+{
+    /// <summary>
+    /// Главный запускающий метод
+    /// </summary>
+    static void Main()
+    {
+
+        Write("Hello "); //Вывод текста на экран
+        Write("World ");
+        Write("!!!");
+
+        ReadLine(); //Ждем нажатия Enter
+    }
+}
